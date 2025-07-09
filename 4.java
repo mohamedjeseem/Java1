e0d2cedbd6445cedbaa831ab186421ba61c468ea
mohamedjeseem <mohamedjeseem.2024cce@sece.ac.in>
2025-07-09 09:27:49 +0530

    Add files via upload

---
+++ 4.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String name,Street,City,Country;
+        name="Jeseem";
+        Street="Market Road";
+        City="Thuckalay";
+        Country="India";
+        System.out.println("My name is = "+name);
+        System.out.println("My street = "+Street);
+        System.out.println("My city = "+City);
+        System.out.println("My country = "+Country);
+    }
+}
