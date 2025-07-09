7b28295347e8177b8c836b9b8906a1d96e563ed6
mohamedjeseem <mohamedjeseem.2024cce@sece.ac.in>
2025-07-09 10:08:58 +0530

    Add files via upload

---
+++ javae.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class javae {
+    public static void main(String[] args) {
+        String name,Street,City,Country;
+        Scanner sc=new Scanner(System.in);
+        name=sc.nextLine();
+        Street=sc.nextLine();
+        City=sc.nextLine();
+        Country=sc.nextLine();
+        
+        System.out.println("My name is = "+name);
+        System.out.println("My street = "+Street);
+        System.out.println("My city = "+City);
+        System.out.println("My country = "+Country);
+        System.out.println("State= "+state);
+    }
+}
