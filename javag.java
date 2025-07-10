dc0560a26d33f96a2191c98c542313d33a600638
mohamedjeseem <mohamedjeseem.2024cce@sece.ac.in>
2025-07-10 14:40:04 +0530

    Add files via upload

---
+++ javag.java
@@ -0,0 +1,16 @@
+package test;                                      
+                                                   
+public class tec {                                 
+                                                   
+	public static void main(String[] args) {       
+		// TODO Auto-generated method stub         
+    for(int i=50;i<=100;i++) {                     
+    	System.out.print(i%2==0?i:" ");            
+    }                                              
+                                                   
+                                                   
+                                                   
+	}                                              
+                                                   
+}                                                  
+                                                   
